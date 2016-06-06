d5483554bc5129dae52af4e2d81d6ce0524130fd
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-06-06 13:29:07 +0530

    Basic setup of restore command

--- ops/command.go
+++ ops/command.go
@@ -1,6 +1,9 @@
 package ops
 
 import (
+	"log"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -11,4 +14,18 @@ var Rocks = &cobra.Command{
 	Long: `Perform common ops related tasks on one or many RocksDB instances.
 
 Find more details at https://github.com/ind9/rocks`,
+}
+
+// CommandHandler is the wrapper interface that all commands to be implement as part of their "Run"
+type CommandHandler func(args []string) error
+
+// AttachHandler is a wrapper method for all commands that needs to be exposed
+func AttachHandler(handler CommandHandler) func(*cobra.Command, []string) {
+	return func(cmd *cobra.Command, args []string) {
+		err := handler(args)
+		if err != nil {
+			log.Printf("[Error] %s", err.Error())
+			os.Exit(1)
+		}
+	}
 }
--- ops/restore.go
+++ ops/restore.go
@@ -1,13 +1,42 @@
 package ops
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var source string
+var destination string
+var walDestinationDir string // generally the same as destination
 
 var restore = &cobra.Command{
 	Use:   "restore",
 	Short: "Restore backed up rocksdb files",
 	Long:  "Restore backed up rocksdb files",
+	Run:   AttachHandler(restoreDatabase),
+}
+
+func restoreDatabase(args []string) error {
+	if source == "" {
+		return fmt.Errorf("--src was not set")
+	}
+	if destination == "" {
+		return fmt.Errorf("--dest was not set")
+	}
+
+	if walDestinationDir == "" {
+		walDestinationDir = destination
+	}
+
+	fmt.Printf("Source=%s, Dest=%s, WAL Dest=%s\n", source, destination, walDestinationDir)
+	return nil
 }
 
 func init() {
 	Rocks.AddCommand(restore)
+
+	restore.PersistentFlags().StringVar(&source, "src", "", "Restore from")
+	restore.PersistentFlags().StringVar(&destination, "dest", "", "Restore to")
+	restore.PersistentFlags().StringVar(&walDestinationDir, "wal-dest", "", "Restore WAL to (generally same as --dest)")
 }
