4ef201053f440ee8e1682f78802b166fd50d97db
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-06-17 16:55:17 +0530

    fixing the repetative fmt.Print statements

--- ops/backup.go
+++ ops/backup.go
@@ -51,7 +51,6 @@ func walkSourceDir(source, destination string) error {
 			dbBackupLoc := filepath.Join(destination, dbRelative)
 			log.Printf("Backup at %s, would be stored to %s\n", dbLoc, dbBackupLoc)
 			log.Printf("Backup is created for %s rocks store", dbRelative)
-			log.Printf("Backup is created for %s rocks store", dbBackupLoc)
 
 			if err = os.MkdirAll(dbBackupLoc, os.ModePerm); err != nil {
 				log.Print(err)
