80c1cfc30ad9fc63cc3ad7db30a199243735155a
Aayush Kumar <iaayush.stevejobs@gmail.com>
2016-07-09 00:22:45 +0530

    Adding DoRecursiveCompaction function

--- ops/compact.go
+++ ops/compact.go
@@ -2,7 +2,11 @@ package ops
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 )
 
@@ -28,4 +32,54 @@ func compactDatabase(args []string) error {
 		return DoRecursiveCompaction(compactionSource, compactionDestination, compactionThreads)
 	}
 	return DoCompaction(compactionSource, compactionDestination)
+}
+
+// DoRecursiveCompaction recursively compacts a rocksdb store keeping the folder structure intact as in source
+func DoRecursiveCompaction(source, destination string, threads int) error {
+
+	workerPool := WorkerPool{
+		MaxWorkers: threads,
+		Op: func(request WorkRequest) error {
+			work := request.(CompactionWork)
+			return DoCompaction(work.Source, work.Destination)
+		},
+	}
+	workerPool.Initialize()
+
+	err := filepath.Walk(source, func(path string, info os.FileInfo, walkErr error) error {
+		if info.Name() == Current {
+			dbLoc := filepath.Dir(path)
+
+			dbRelative, err := filepath.Rel(source, dbLoc)
+			if err != nil {
+				log.Print(err)
+				return err
+			}
+
+			dbCompactionLoc := filepath.Join(destination, dbRelative)
+			if err = os.MkdirAll(dbCompactionLoc, os.ModePerm); err != nil {
+				log.Print(err)
+				return err
+			}
+
+			work := CompactionWork{
+				Source:      dbLoc,
+				Destination: dbCompactionLoc,
+			}
+			workerPool.AddWork(work)
+			return filepath.SkipDir
+		}
+		return walkErr
+	})
+
+	var result error
+	if errFromWorkers := workerPool.Join(); errFromWorkers != nil {
+		result = multierror.Append(result, errFromWorkers)
+	}
+
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+
+	return result
 }
