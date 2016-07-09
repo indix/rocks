0ca1554a3669e890d2c42fea27db48df13decdb0
Aayush Kumar <iaayush.stevejobs@gmail.com>
2016-07-09 20:18:27 +0530

    Adding TestRecursiveCompaction function

--- ops/compact_test.go
+++ ops/compact_test.go
@@ -3,6 +3,7 @@ package ops
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,5 +16,26 @@ func TestCompact(t *testing.T) {
 
 	WriteTestDB(t, dataDir)
 	err = DoCompaction(dataDir)
+	assert.NoError(t, err)
+}
+
+func TestRecursiveCompaction(t *testing.T) {
+	baseDataDir, err := ioutil.TempDir("", "baseDataDir")
+	err = os.MkdirAll(baseDataDir, os.ModePerm)
+	defer os.RemoveAll(baseDataDir)
+	assert.NoError(t, err)
+
+	paths := []string{
+		"1/store_1/",
+		"1/store_2/",
+		"2/store_1/",
+		"2/store_2/",
+	}
+
+	for _, relLocation := range paths {
+		WriteTestDB(t, filepath.Join(baseDataDir, relLocation))
+	}
+
+	err = DoRecursiveCompaction(baseDataDir, 1)
 	assert.NoError(t, err)
 }
