10b45c882f9dd99978df9197a3fa72e92e1c4365
Aayush Kumar <iaayush.stevejobs@gmail.com>
2016-07-12 15:59:08 +0530

    Adding DoConsistency function

--- ops/consistancy.go
+++ ops/consistancy.go
@@ -2,12 +2,15 @@ package ops
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
+	"github.com/tecbot/gorocksdb"
 )
 
 var consistancySource string
 var consistancyRestore string
+var flag bool
 
 var consistency = &cobra.Command{
 	Use:   "consistency",
@@ -26,4 +29,40 @@ func checkConsistency(args []string) error {
 	}
 
 	return DoConsistency(consistancySource, consistancyRestore)
+}
+
+// DoConsistency checks for consistency between rocks source store and its restore
+func DoConsistency(source, restore string) error {
+	log.Printf("Initializing consistency check between %s rocks store and %s as it's restore\n", source, restore)
+
+	opts := gorocksdb.NewDefaultOptions()
+	dbSource, err := gorocksdb.OpenDb(opts, source)
+	defer dbSource.Close()
+	dbRestore, err := gorocksdb.OpenDb(opts, restore)
+	defer dbRestore.Close()
+
+	consistencyOpts := gorocksdb.NewDefaultReadOptions()
+	consistencyOpts.SetFillCache(false)
+	sourceIterator := dbSource.NewIterator(consistencyOpts)
+	restoreIterator := dbRestore.NewIterator(consistencyOpts)
+
+	var rowCountSource, rowCountRestore int64
+	log.Printf("Trying to collect the stores with non-matching number of keys\n")
+	flag = true
+	for sourceIterator.SeekToFirst(); sourceIterator.Valid(); sourceIterator.Next() {
+		rowCountSource++
+	}
+	for restoreIterator.SeekToFirst(); restoreIterator.Valid(); restoreIterator.Next() {
+		rowCountRestore++
+	}
+	if rowCountSource != rowCountRestore {
+		fmt.Printf("Store : %s and corresponding Restore %s number of keys did not match\n", source, restore)
+		fmt.Printf("Store Count : %v\n", rowCountSource)
+		fmt.Printf("Restore Count : %v\n", rowCountRestore)
+		flag = false
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
