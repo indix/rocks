e9fcc6621e74459808de4fb553ffb97870842798
Aayush Kumar <iaayush.stevejobs@gmail.com>
2016-07-12 16:34:34 +0530

    Adding boolean support to DoConsistency function

--- ops/consistancy.go
+++ ops/consistancy.go
@@ -10,6 +10,7 @@ import (
 
 var consistancySource string
 var consistancyRestore string
+var consistancyThreads int
 var flag bool
 
 var consistency = &cobra.Command{
@@ -19,7 +20,7 @@ var consistency = &cobra.Command{
 	Run:   AttachHandler(checkConsistency),
 }
 
-func checkConsistency(args []string) error {
+func checkConsistency(args []string) (err error) {
 	if consistancySource == "" {
 		return fmt.Errorf("--src was not set")
 	}
@@ -27,12 +28,28 @@ func checkConsistency(args []string) error {
 	if consistancyRestore == "" {
 		return fmt.Errorf("--dest was not set")
 	}
+	var checkConsistant bool
+	if recursive {
+		checkConsistant, err = DoRecursiveConsistency(consistancySource, consistancyRestore, consistancyThreads)
+	} else {
+		checkConsistant, err = DoConsistency(consistancySource, consistancyRestore)
+	}
+	if checkConsistant {
+		fmt.Printf("Store and Restore are consistant")
+	}
+	return err
+}
 
-	return DoConsistency(consistancySource, consistancyRestore)
+// DoRecursiveConsistency checks for consistency recursively
+func DoRecursiveConsistency(source, restore string, threads int) (bool, error) {
+	/*
+	   Implement this piece of code
+	*/
+	return false, nil
 }
 
 // DoConsistency checks for consistency between rocks source store and its restore
-func DoConsistency(source, restore string) error {
+func DoConsistency(source, restore string) (bool, error) {
 	log.Printf("Initializing consistency check between %s rocks store and %s as it's restore\n", source, restore)
 
 	opts := gorocksdb.NewDefaultOptions()
@@ -56,13 +73,13 @@ func DoConsistency(source, restore string) error {
 		rowCountRestore++
 	}
 	if rowCountSource != rowCountRestore {
-		fmt.Printf("Store : %s and corresponding Restore %s number of keys did not match\n", source, restore)
-		fmt.Printf("Store Count : %v\n", rowCountSource)
-		fmt.Printf("Restore Count : %v\n", rowCountRestore)
+		log.Printf("Store : %s and corresponding Restore %s number of keys did not match\n", source, restore)
+		log.Printf("Store Count : %v\n", rowCountSource)
+		log.Printf("Restore Count : %v\n", rowCountRestore)
 		flag = false
 	}
 	if err != nil {
-		return err
+		return flag, err
 	}
-	return nil
+	return flag, err
 }
