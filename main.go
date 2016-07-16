40397ab65fc396bc2706bf359325625845aace93
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-07-16 18:27:52 +0530

    Importing all the commands in the main so they're all available

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	_ "github.com/ind9/rocks/cmd/backup"
+	_ "github.com/ind9/rocks/cmd/consistency"
 	"github.com/ind9/rocks/cmd/ops"
+	_ "github.com/ind9/rocks/cmd/restore"
+	_ "github.com/ind9/rocks/cmd/statistics"
+	_ "github.com/ind9/rocks/cmd/trigger"
 )
 
 // Version of the app
